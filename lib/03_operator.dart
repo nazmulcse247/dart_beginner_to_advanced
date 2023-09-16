5e047aaa32e8ef4d65fb4454f568de510baa8590
nazmulcse247 <nazmulhoque247@gmail.com>
2023-09-16 22:47:37 +0600

    constrants

--- lib/03_operator.dart
+++ lib/03_operator.dart
@@ -18,7 +18,7 @@ void main() {
   /*In Java, there are ++ and -- operators are known as increment and decrement operators and also known as unary operators, respectively.
    Unary operators, operate on single operand where ++ adds 1 to operands and -- subtract 1 to operand respectively.*/
 
-  print("----------------unary operator---------------");
+  print("------------Example Unary operator----------");
 
   int c = 10;
   print(c++); //postfix value
@@ -40,6 +40,51 @@ void main() {
 
   g += h;  //g = g + h
   print("g += h = $g");
+
+  //Relational operator
+  print("--------Example of Relational operators-------");
+
+  int i = 30;
+  int j = 20;
+
+  var result = i > j;
+  print("i is greater than j: $result");
+
+  bool result2 = i < j;
+  print("i is less than j: $result2");
+
+  var result3 = i >= j;
+  print("i is greater than or equal to j: $result3");
+
+  var result4 = i <= j;
+  print("i is less than or equal to j: $result4");
+
+  var result5 = i == j;
+  print("i is equal to j: $result5");
+
+  var result6 = i != j;
+  print("i is not equal to j: $result6");
+
+  //Logical operator
+  print("--------Example of Logical operators-------");
+
+  int k = 10;
+  int l = 20;
+
+  var result7 = (k > l) && (k != l);
+  print("(k > l) && (k != l): $result7");
+
+  var result8 = (k < l) || (k == l);
+  print("(k < l) || (k == l): $result8");
+
+  //Ternary operator
+  print("--------Example of Ternary operators-------");
+
+  var m = null;
+  int n = 20;
+
+  int mn = (m == null) ? n : m;
+  print("mn: $mn");
 
 
 
---
+++ lib/04_constants.dart
@@ -0,0 +1,9 @@
+void main(){
+  // dart constants
+
+  final String name = "John Doe";
+  const age = 30;
+
+  print("Name : $name and Age : $age");
+
+}
