4b7e9f93ed672872d1f1ae74b9334fd45c66b03f
nazmulcse247 <nazmulhoque247@gmail.com>
2023-09-16 13:52:50 +0600

    variable declaration

---
+++ android/app/src/main/kotlin/com/example/dart_beginner_to_advanced/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dart_beginner_to_advanced
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/02_variable.dart
@@ -0,0 +1,19 @@
+// declare variable with type & without type
+void main() {
+
+  // declare variable with type
+  String name = "John Doe";
+  int age = 30;
+  double height = 1.8;
+  bool isMarried = false;
+
+  // declare variable without type
+  var address = "Jl. Raya Kebayoran Lama No. 12";
+  var phoneNumber = 6281234567890;
+  var weight = 70.5;
+  var isSingle = true;
+
+
+  // print variable
+
+}
