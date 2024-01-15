77ac8b0478d08416d8750205d5f455bb8441f352
nazmulcse247 <nazmulhoque247@gmail.com>
2024-01-15 16:50:18 +0600

    modify HOF

--- lib/11_higher_order_function.dart
+++ lib/11_higher_order_function.dart
@@ -1,11 +1,19 @@
-void main(){
-
-  List<int> numbers = [10,20,30];
-  myHigherOrderFuntion(10, 20, (){
-    print("annonymous funtion");
-  });
+// Higher-order function that takes two values and a function, and returns the result
+dynamic addValuesWithFunction(dynamic a, dynamic b, dynamic Function(dynamic, dynamic) operation) {
+  return operation(a, b);
 }
 
-void myHigherOrderFuntion(int a, int b, Function action){
-  action();
+void main() {
+  // Define a function for addition
+  //dynamic addFunction(dynamic x, dynamic y) => x + y;
+
+  // Use the higher-order function to add two values using the specified function
+  dynamic result = addValuesWithFunction(3, 4, (x, y) => x + y);
+  print("Result of addition: $result"); // Output: Result of addition: 7
 }
+
+
+
+
+
+
